0a82b92dbf146db43df7f3fb5d45d9d37edf34e0
Naveen63823696 <naveen46mv@gmail.com>
2025-12-31 14:12:12 +0530

    Fix controller import case for Render deployment

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.use("/api/paste", require("./routes/pasteRoutes"));
+app.use("/api/paste", require("./routes/PasteRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
